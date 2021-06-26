89d5ab4e6e411511428a5017d3d5077ba48c98e6
Dener Troquatte <dener@vidafullstack.com.br>
2021-06-26 14:10:40 -0300

    Validators

---
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent implements OnInit {
+
+  public cadastroForm: FormGroup = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  public submitForm() {
+    console.log(this.cadastroForm.value);
+    console.log(this.cadastroForm.value.firstName);
+    console.log(this.cadastroForm.value.lastName);
+  }
+}
